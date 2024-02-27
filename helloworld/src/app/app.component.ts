1eaeb0059ec719e5986e5c4f83ee7a5a290822d0
naman995 <rajputnaman823@gmail.com>
2024-02-28 00:45:06 +0530

    HostListener

---
+++ helloworld/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+
+})
+export class AppComponent {
+  title = 'helloworld';
+   name:string = "Hello World form app";
+}
---
+++ helloworld/src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, HostListener } from '@angular/core';
+import { Demo2Component } from '../demo2/demo2.component';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [Demo2Component],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+})
+export class CardComponent {
+  @HostListener('click', ['$event'])
+  show() {
+    console.log('CardComponent clicked');
+  }
+}
