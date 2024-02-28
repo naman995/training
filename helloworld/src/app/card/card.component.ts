aa655616ea58fceb0dc9d1d9344ee21a6c72e3f8
naman995 <rajputnaman823@gmail.com>
2024-02-29 00:27:01 +0530

    preserve whitespace

--- helloworld/src/app/card/card.component.ts
+++ helloworld/src/app/card/card.component.ts
@@ -2,15 +2,13 @@ import { Component, HostListener } from '@angular/core';
 import { Demo2Component } from '../demo2/demo2.component';
 
 @Component({
-  selector: '.app-card',
+  selector: 'app-card',
   standalone: true,
   imports: [Demo2Component],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
+  preserveWhitespaces: true,
 })
 export class CardComponent {
-  @HostListener('click', ['$event'])
-  show() {
-    console.log('CardComponent clicked');
-  }
+ 
 }
